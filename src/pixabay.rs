257010eaaf1909615489106634029755dc026951
Rouven Himmelstein <rouven@himmelstein.info>
2022-06-18 11:46:44 +0200

    feat: improve doc

--- src/pixabay.rs
+++ src/pixabay.rs
@@ -4,14 +4,27 @@ use crate::config::Config;
 use crate::display::DisplayInfo;
 use crate::file_receiver;
 
+/// pixabay api base url
 const BASE_URL: &str =
     "https://pixabay.com/api/?key=15495421-a5108e860086b11eddaea0efa&per_page=25";
 
+/// Gets an image from pixabay
+/// # Arguments
+/// * `config` - The configuration to use
+/// * `display_info` - The display info to use
+/// # Returns
+/// The image data as byte array
 pub fn get_image_data(config: &Config, display_info: &DisplayInfo) -> Vec<u8> {
     let image_url = get_image_url(config, display_info);
     file_receiver::download_data(&image_url)
 }
 
+/// Gets the image url from pixabay
+/// # Arguments
+/// * `config` - The configuration to use
+/// * `display_info` - The display info to use
+/// # Returns
+/// A random image url from pixabay response
 fn get_image_url(config: &Config, display_info: &DisplayInfo) -> String {
     let request_url = build_request_url(config, display_info);
     let json_string = download_as_string(&request_url);
@@ -30,11 +43,22 @@ fn get_image_url(config: &Config, display_info: &DisplayInfo) -> String {
     return images.get(random_index).unwrap().to_string();
 }
 
+/// Downloads a string from a url
+/// # Arguments
+/// * `url` - The url to download
+/// # Returns
+/// The downloaded string as utf 8
 fn download_as_string(request_url: &String) -> String {
     let data = file_receiver::download_data(request_url);
     String::from_utf8(data).unwrap()
 }
 
+/// Builds the request url
+/// # Arguments
+/// * `config` - The configuration to use
+/// * `display_info` - The display info to use
+/// # Returns
+/// The request url as string
 fn build_request_url(config: &Config, display_info: &DisplayInfo) -> String {
     let mut target_width = get_width(&display_info.max_single_resolution);
     if config.span {
@@ -54,10 +78,21 @@ fn build_request_url(config: &Config, display_info: &DisplayInfo) -> String {
     request_url
 }
 
+/// Appends a string to anoher string
 fn append_str(base_string: &mut String, str1: &str, str2: &str) {
     base_string.push_str(&[str1, str2].join(""))
 }
 
+/// Gets the width of a resolution string
+/// # Arguments
+/// * `resolution` - The resolution string
+/// # Returns
+/// The width of the resolution string
+/// # Example
+/// ```
+/// use image_edit::get_width;
+/// assert_eq!(get_width("1920x1080"), 1920);
+/// ```
 fn get_width(resolution_string: &str) -> String {
     return resolution_string
         .split('x')
@@ -66,6 +101,16 @@ fn get_width(resolution_string: &str) -> String {
         .to_string();
 }
 
+/// Gets the height of a resolution string
+/// # Arguments
+/// * `resolution` - The resolution string
+/// # Returns
+/// The height of the resolution string
+/// # Example
+/// ```
+/// use image_edit::get_height;
+/// assert_eq!(get_height("1920x1080"), 1080);
+/// ```
 fn get_height(resolution_string: &str) -> String {
     return resolution_string
         .split('x')
